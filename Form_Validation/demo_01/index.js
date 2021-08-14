337a2bf6eb42fa219477c7272412817bbeb01c8f
KayJuno <dangdan2807@gmail.com>
2021-08-14 23:36:43 +0700

    update: Form Validation - demo 1 add input: gender, target, province, upload file

--- Form_Validation/demo_01/index.js
+++ Form_Validation/demo_01/index.js
@@ -1,8 +1,8 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
 // object `Validator`
 function Validator(options) {
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
+
     var selectorRules = {};
 
     // user input
@@ -19,16 +19,26 @@ function Validator(options) {
         // get rule & check
         // if have error => break
         for (let i = 0; i < rules.length; i++) {
-            errorMessage = rules[i](inputElement.value);
+            switch (inputElement.type) {
+                case "radio":
+                case "checkbox":
+                    errorMessage = rules[i](formElement.querySelector(rule.selector + ":checked"));
+                    break;
+                default:
+                    errorMessage = rules[i](inputElement.value);
+                    break;
+            }
             if (errorMessage) break;
         }
 
         if (errorMessage !== undefined) {
             errorElement.innerText = errorMessage;
-            inputElement.parentElement.classList.add("invalid");
+            inputElement.closest(options.formGroupSelector).classList.add("invalid");
         } else {
             userInput(inputElement, errorElement);
         }
+
+        return !errorMessage;
     }
 
     // get form element to validate
@@ -36,11 +46,51 @@ function Validator(options) {
     if (formElement) {
         formElement.onsubmit = function (e) {
             e.preventDefault();
+            var isFormValid = true;
+
             options.rules.forEach(function (rule) {
                 var inputElement = formElement.querySelector(rule.selector);
-                var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-                validate(inputElement, errorElement, rule);
+                var errorElement = inputElement.closest(options.formGroupSelector).querySelector(options.errorSelector);
+                var isValid = validate(inputElement, errorElement, rule);
+
+                if (!isValid) {
+                    isFormValid = false;
+                }
             });
+
+            if (isFormValid) {
+                if (typeof options.onSubmit === "function") {
+                    var enableInputs = formElement.querySelectorAll("[name]:not([disabled])");
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        switch (input.type) {
+                            case "checkbox":
+                                if (!input.matches(":checked")) {
+                                    values[input.name] = [];
+                                    return values;
+                                }
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case "radio":
+                                if (input.matches(":checked")) {
+                                    values[input.name] = input.value;
+                                }
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                                break;
+                        }
+                        return values;
+                    }, {});
+
+                    options.onSubmit(formValues);
+                }
+            }
         };
 
         options.rules.forEach(function (rule) {
@@ -53,28 +103,33 @@ function Validator(options) {
                 selectorRules[rule.selector] = [rule.test];
             }
 
-            var inputElement = formElement.querySelector(rule.selector);
-            var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-            // handle blur from input
-            if (inputElement) {
-                inputElement.onblur = function () {
-                    validate(inputElement, errorElement, rule);
-                };
-            }
+            var inputElements = formElement.querySelectorAll(rule.selector);
+            Array.from(inputElements).forEach(function (inputElement) {
+                var errorElement = inputElement.closest(options.formGroupSelector).querySelector(options.errorSelector);
+                // handle blur from input
+                if (inputElement) {
+                    inputElement.onblur = function () {
+                        validate(inputElement, errorElement, rule);
+                    };
+                }
 
-            // handle user input
-            inputElement.oninput = function () {
-                userInput(inputElement, errorElement);
-            };
+                // handle user input
+                inputElement.oninput = function () {
+                    userInput(inputElement, errorElement);
+                };
+            });
         });
     }
 }
 
-Validator.isRequired = function (selector, message = "value input") {
+Validator.isRequired = function (selector, lastMessage = "value input", firstMessage = "enter") {
     return {
         selector: selector,
         test: function (value) {
-            return value.trim() ? undefined : `You must enter ${message}`;
+            if (value === "string") {
+                return value.trim() ? undefined : `You must ${firstMessage} ${lastMessage}`;
+            }
+            return value ? undefined : `You must ${firstMessage} ${lastMessage}`;
         },
     };
 };
@@ -107,13 +162,18 @@ Validator.isConfirmed = function (selector, getConfirmValue, message) {
     };
 };
 
+const $ = document.querySelector.bind(document);
+
 Validator({
     form: "#form-1",
     errorSelector: ".form-message",
+    formGroupSelector: ".form-group",
     rules: [
-        Validator.isRequired("#fullName", "your full name"),
         Validator.isRequired("#email", "your email"),
         Validator.isEmail("#email"),
+        Validator.isRequired("#fullName", "your full name"),
+        Validator.isRequired("input[name='gender']", "your gender"),
+        Validator.isRequired("#province", "your province", 'choose'),
         Validator.isRequired("#password", "password"),
         Validator.minLength("#password", 6, "Password"),
         Validator.isRequired("#password_confirmation", "password"),
@@ -124,5 +184,9 @@ Validator({
             },
             "Re-entered password is incorrect"
         ),
+        Validator.isRequired("#avatar", "your avatar", "upload"),
     ],
+    onSubmit: function (data) {
+        console.log(data);
+    },
 });
